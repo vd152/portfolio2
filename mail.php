16ea801dbf9c2a368a9ad24fc4834b4285e64a5c
Vidhi <vidhi152@gmail.com>
2020-08-10 14:15:51 +0530

    contact form

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php 
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "vidhi152@gmail.com";
+$subject = $_POST['subject'];
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
